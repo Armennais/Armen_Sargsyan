11163cd80ff98c46e2d511c3c9e8d436170250b3
Armen <you@example.com>
2022-03-03 21:35:44 +0300

    Added c++ homework 5

---
+++ cpp/day_1/homework_5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,width,perimeter,area ;
+    cout << "Input edges of rectangle : \n"; 
+    cin >> length >> width; // Input length and width at once
+    perimeter = 2*(length+width);//calculate the perimeter of rectangle and assigns on variable
+    area = length*width;//calculate the area of rectangle and assigns on variable
+    cout << "Perimeter = "<< perimeter << "    " << "Area = " << area<< endl;
+    return 0;
+}
