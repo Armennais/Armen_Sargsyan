5cf10139a19f1c75dc102728d89b512709aa1e36
Armen <you@example.com>
2022-03-03 20:00:25 +0300

    Added c++ homework_1

---
+++ cpp/day_1/homework_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s_num; // secret number
+    int number;
+    cout << "Input secret number for guessing in [0:100] range \n";
+    cin >> s_num ; //Input secret number
+    do {
+        if (s_num < 0 || s_num > 100){
+            cout << "I said in [0:100] range!!! \n";
+            cin >> s_num;
+        } else {
+            cout << "Try guess the number : \n";
+            cin >> number;
+            if (number < s_num){
+                cout << "Try a larger number \n";
+            } else if (number > s_num){
+                cout << "Try a smaller number \n";
+            }else {
+                cout << "YESSS!!!  \n";
+            }
+        }
+    } while (number != s_num);
+
+    return 0;
+}
