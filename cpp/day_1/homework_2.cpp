7bc6d0b0827bdf6252561bbc00470d09a8e1c6cb
Armen <you@example.com>
2022-03-03 20:20:27 +0300

    Added c++ homework 2

---
+++ cpp/day_1/homework_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int mult_num;
+    cout << "Input multiple number : ";
+    cin >> mult_num; //Input number to print its table
+    int c = 0;
+    int limit = 10;
+    while(c<=limit){
+        cout << mult_num << " * " << c << " = " << c*mult_num<<endl; //Print multiplication table 
+        c++;
+    }
+    
+}
