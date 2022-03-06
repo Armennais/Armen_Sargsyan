5105dbcfc8494d775f4e548c7e0ca44d8b909f00
Armen <you@example.com>
2022-03-06 20:07:15 +0300

    Corrected 2nd day 3rd homework

--- cpp/day_2/3_alphabet_check.cpp
+++ cpp/day_2/3_alphabet_check.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  char letter;
-  cout << "Enter letter from  a to Z := ";
-  cin >> letter;
-  switch (letter)
-  {
-    case 'A':
-    case 'E':
-    case 'I':
-    case 'O':
-    case 'U':
-    case 'a':
-    case 'e':
-    case 'i':
-    case 'o':
-    case 'u':
-      cout << "'" << letter << "' is vowel \n";
-      break;
+	char letter;
+	cout << "Enter letter from  a to Z := ";
+	cin >> letter;
+	if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z'))
+	{
+		switch (letter)
+		{
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				cout << "'" << letter << "' is vowel \n";
+				break;
 
-    default:
-      cout << "'" << letter << "' is consonant \n";
-  }
+			default:
+				cout << "'" << letter << "' is consonant \n";
+		}
+	}
+	else
+	{
+		cout << letter << " is not letter.\n";
+	}
 
-  return 0;
+	return 0;
 
 }
