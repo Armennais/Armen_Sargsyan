843ffc6605551d79a6aae17604dcee5c1f02d989
Armen <you@example.com>
2022-03-10 01:04:52 +0300

    Chenged any errors on this homework

--- cpp/day_4/5_reversed_array.cpp
+++ cpp/day_4/5_reversed_array.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void sorted_array(int);
+void reversed_array(int);
 
 int main()
 {
   int num;
   cout << "Enter a size: ";
   cin >> num;
-  sorted_array(num);
+    reversed_array(num);
   return 0;
 }
 
-void sorted_array(int num)
+void reversed_array(int num)
 {
-  int randArray[num];
+      int randArray[num];
 
   for (int i = 0; i < num; i++)
-    randArray[i] = rand() % 100;
+  randArray[i] = rand() % 100;
   cout << "Random array is: ";
   for (int i = 0; i < num; i++)
     cout << randArray[i] << " ";
 
-  for (int i = 0; i < num; i++)
-  {
-    for (int j = 1 + i; j < num; j++)
-    {
-      if (randArray[i] > randArray[j])
-      {
-        int temp = randArray[i];
-        randArray[i] = randArray[j];
-        randArray[j] = temp;
-      }
-    }
-  }
-
-  cout << "\nSorted array is: ";
-  for (int i = 0; i < num; i++)
+  cout << "\nReversed array is: ";
+  for (int i = num - 1; i >= 0; i--)
   {
     cout << randArray[i] << " ";
   }
