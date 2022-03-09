a8efc3fff24f62e198c56d816520a3433412de46
Armen <you@example.com>
2022-03-09 20:46:43 +0300

    Added day 4 homework 3

---
+++ cpp/day_4/3_array_maxNum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void max_val_array(int);
+int find_max(int, int randArray[]);
+
+int main()
+{
+  int num;
+  cout << "Enter the size of array: ";
+  cin >> num;
+  max_val_array(num);
+  return 0;
+}
+
+void max_val_array(int num)
+{
+  int randArray[num];
+  for (int i = 0; i < num; i++)
+    randArray[i] = rand() % 100;
+  cout << "Random array is:[";
+  for (int i = 0; i < num; i++)
+    cout << randArray[i] << " ";
+  cout << "]; ";
+
+  cout << endl << "Largest element = " << find_max(num, randArray) << ";\n";
+}
+
+int find_max(int num, int randArray[])
+{
+  for (int i = 1; i < num; ++i)
+  {
+    if (randArray[0] < randArray[i])
+      randArray[0] = randArray[i];
+  }
+
+  return randArray[0];
+}
