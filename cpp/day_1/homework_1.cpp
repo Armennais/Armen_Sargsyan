a449a29856227f5f93273d72a70c7bb0a43c18f6
Armennais <armensargsyan1920@gmail.com>
2022-04-03 20:33:24 +0400

    Corrected coding style

--- cpp/day_1/homework_1.cpp
+++ cpp/day_1/homework_1.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 using namespace std;
+void number_Guessing(int );//Function guess number on [0:100 ]range
 
 int main()
 {
-  int s_num;  // secret number
-  int number;
-  cout << "Input secret number for guessing in[0:100] range \n";
-  cin >> s_num;  //Input secret number
-  do {
-    if (s_num < 0 || s_num > 100)
-    {
-      cout << "I said in[0:100] range!!! \n";
-      cin >> s_num;
-    }
-    else
-    {
-      cout << "Try guess the number : \n";
-      cin >> number;
-      if (number < s_num)
-      {
-        cout << "Try a larger number \n";
-      }
-      else if (number > s_num)
-      {
-        cout << "Try a smaller number \n";
-      }
-      else
-      {
-        cout << "YESSS!!!  \n";
-      }
-    }
-  } while (number != s_num);
-
-  return 0;
+	int secret_num;  // secret number
+	cout << "Input secret number for guessing in[0:100] range \n";
+	cin >> secret_num;  //Input secret number
+	number_Guessing(secret_num);
+	return 0;
+}
+//function that guess number
+void number_Guessing(int secret_num)
+{
+	int guess_number;
+	do {
+		if (secret_num < 0 || secret_num > 100)
+		{
+			cout << "I said in[0:100] range!!! \n";
+			cin >> secret_num;
+			cin.ignore();
+		}
+		else
+		{
+			cout << "Try guess the number : ";
+			cin >> guess_number;
+			if (guess_number < secret_num)
+				cout << "Try a larger number \n";
+			else if (guess_number > secret_num)
+				cout << "Try a smaller number \n";
+			else
+				cout << "YESSS!!!  \n";
+		}
+	} while (guess_number != secret_num);
 }
