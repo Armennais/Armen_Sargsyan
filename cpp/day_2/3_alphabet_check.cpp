04f1df107955c8868c95e0b53df8c1a727020735
Armennais <armensargsyan1920@gmail.com>
2022-04-04 19:23:45 +0400

    Update codding style

--- cpp/day_2/3_alphabet_check.cpp
+++ cpp/day_2/3_alphabet_check.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	char letter;
+input:
 	cout << "Enter letter from  a to Z := ";
 	cin >> letter;
 	if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z'))
@@ -29,6 +30,8 @@ int main()
 	else
 	{
 		cout << letter << " is not letter.\n";
+		cout << "Please ";
+		goto input;
 	}
 	return 0;
 
