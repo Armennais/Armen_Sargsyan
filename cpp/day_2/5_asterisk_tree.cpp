12d619ddbaff3f34fb988f755bd9fcf33a4fca34
Armennais <armensargsyan1920@gmail.com>
2022-04-04 21:58:13 +0400

    Update codding style

--- cpp/day_2/5_asterisk_tree.cpp
+++ cpp/day_2/5_asterisk_tree.cpp
@@ -3,131 +3,116 @@ using namespace std;
 
 int main()
 {
-  int c, i = 0;
-  cout << "Enter count: \n";
-  cin >> c;
-  const int count = c;
-  i = count;
+	int c, i = 0;
+	cout << "Enter count: \n";
+      	cin >> c;
+	const int count = c;
+	i = count;
   
-  //Asterisk example 1
-  
-  while (i > 0)
-  {
-    int j = 0;
-    while (j < i)
-    {
-      cout << " *";
-      j++;
-    }
+	//Asterisk example 1
+	
+	while (i > 0)
+	{
+		int j = 0;
+		while (j < i)
+		{
+			cout << " *";
+			j++;
+		}
 
-    j = 0;
+    	j = 0;
+    	cout << " " << endl;
+    	i--;	
+	}
 
-    cout << " " << endl;
-
-    i--;
-  }
 
   //Asterisk example 2
 
-  while (i < count)
-  {
-    int j = 0;
-    while (j <= i)
-    {
-      cout << " *";
-      j++;
-    }
-
-    j = 0;
-    cout << " " << endl;
-    i++;
-  }
-
-  i = 0;
+  	while (i < count)
+  	{
+    		int j = 0;
+    		while (j <= i)
+    		{
+      			cout << " *";
+      			j++;
+    		}
+    		j = 0;
+    		cout << " " << endl;
+    		i++;
+  	}	
+  	i = 0;
 
   //Asterisk example 3
 
-  while (i < count)
-  {
-    int j = 0;
+  	while (i < count)
+  	{
+    		int j = 0;
+   		while (j < count - i)
+    		{	
+      			cout << " ";
+      			j++;
+    		}
+    		j = 0;
+    		while (j <= i)
+    		{
+      			cout << "*";
+      			j++;
+    		}
+    		j = 0;
+    		i++;
 
-    while (j < count - i)
-    {
-      cout << " ";
-      j++;
-    }
+    		cout << " " << endl;
+  	}
 
-    j = 0;
-    while (j <= i)
-    {
-      cout << "*";
-      j++;
-    }
-
-    j = 0;
-
-    i++;
-
-    cout << " " << endl;
-  }
-
-  i = 0;
+  	i = 0;
 
 
   //Another Asterisk example 3
   
   
-  while (i < count)
-  {
-    int j = 0;
+  	while (i < count)
+  	{
+    		int j = 0;
 
-    while (j < count - i)
-    {
-      cout << " ";
-      j++;
-    }
+    		while (j < count - i)
+    		{
+      			cout << " ";
+      			j++;
+    		}
 
-    j = 0;
-    while (j <= i)
-    {
-      cout << " *";
-      j++;
-    }
-
-    j = 0;
-
-    i++;
-
-    cout << " " << endl;
-  }
+    		j = 0;
+    		while (j <= i)
+    		{
+      			cout << " *";
+      			j++;
+    		}
+    		j = 0;
+    		i++;
+    		cout << " " << endl;
+  	}
 
   i = 0;
 
   //Asterisk example 4
 
-  while (i < count)
-  {
-    int j = 0;
-
-    while (j < count - i)
-    {
-      cout << " ";
-      j++;
-    }
-
-    j = 0;
-
-    while (j <= 2 *i)
-    {
-      cout << "*";
-      j++;
-    }
-
-    j = 0;
-    i++;
-
-    cout << " " << endl;
-  }
+  	while (i < count)
+  	{
+    		int j = 0;
+    		while (j < count - i)
+    		{
+      			cout << " ";
+      			j++;
+    		}
+    		j = 0;
+    		while (j <= 2 *i)
+    		{
+      			cout << "*";
+      			j++;
+    		}
+    		j = 0;
+    		i++;
+    		cout << " " << endl;
+  	}
 
   return 0;
 }
