50da03db12ac99b5c65c6643e76476181a8b24d2
Armennais <armensargsyan1920@gmail.com>
2022-04-04 19:18:04 +0400

    Update coding style

--- cpp/day_2/1_week_number.cpp
+++ cpp/day_2/1_week_number.cpp
@@ -3,35 +3,36 @@ using namespace std;
 int main()
 {
   int weeknumber;
-
+input:
   cout << "Enter week number(1-7): ";
   cin >> weeknumber;
 
   switch (weeknumber)
   {
-    case 1:
-      cout << "Mondayi \n";
-      break;
-    case 2:
-      cout << "Tuesday \n";
-      break;
-    case 3:
-      cout << "Wednesday \n";
-      break;
-    case 4:
-      cout << "Thursday \n";
-      break;
-    case 5:
-      cout << "Friday \n";
-      break;
-    case 6:
-      cout << "Saturday \n";
-      break;
-    case 7:
-      cout << "Sunday \n";
-      break;
-    default:
-      cout << "Please enter between 1-7. \n";
+	case 1:
+      		cout << "Monday \n";
+      		break;
+    	case 2:
+      		cout << "Tuesday \n";
+      		break;
+    	case 3:
+      		cout << "Wednesday \n";
+      		break;
+    	case 4:
+      		cout << "Thursday \n";
+      		break;
+    	case 5:
+      		cout << "Friday \n";
+      		break;
+    	case 6:
+      		cout << "Saturday \n";
+      		break;
+    	case 7:
+      		cout << "Sunday \n";
+      		break;
+	default:
+      		cout << "Please enter between 1-7. \n";
+      		goto input;		
   }
 
   return 0;
