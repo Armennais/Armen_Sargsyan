a99994797cb7754193e17a71a7fb5684e0ce2d61
Armennais <armensargsyan1920@gmail.com>
2022-06-21 16:35:10 +0400

    Make update

--- js/04_day/example.js
+++ js/04_day/example.js
@@ -3,4 +3,4 @@ let id = 0;
 db.createTable("user",["username","password"],["Armen1","asd"])
 db.createTable("user1",["username","password"],["Karen","asdfgd"])
 db.createTable("user2",["username","password"],["Vazgen","aghsdfgd"])
-console.log(db.getTable("user"))bash
+console.log(db.getTable("user"))
