13abfc987e47b19cc3c00d42e42b9702272e98d7
Armennais <armensargsyan1920@gmail.com>
2022-06-21 23:59:35 +0400

    Make update

--- js/04_day/example.js
+++ js/04_day/example.js
@@ -1,6 +1,12 @@
 const db = Database.get();
 let id = 0;
-db.createTable("user",["username","password"],["Armen1","asd"])
-db.createTable("user1",["username","password"],["Karen","asdfgd"])
-db.createTable("user2",["username","password"],["Vazgen","aghsdfgd"])
-console.log(db.getTable("user"))
+db.createTable("user",["username","password"],[["Armen1","asd"]]);
+db.createTable("user1",["username","password"],["Karen","asdfgd"]);
+db.createTable("user2",["username","password"],["Vazgen","aghsdfgd"]);
+
+const a = new Table();
+a.add(1,"Vazgenchik","aedaef");
+a.update(1,"Gagik","aedefa");
+a.delete(2);
+
+
