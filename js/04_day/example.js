fd33c7c5a7b68b19fc5618ad34d692791c83b738
Armen Sargsyan <91330439+Armennais@users.noreply.github.com>
2022-06-22 09:44:49 +0400

    Update example.js

--- js/04_day/example.js
+++ js/04_day/example.js
@@ -4,9 +4,8 @@ db.createTable("user",["username","password"],[["Armen1","asd"]]);
 db.createTable("user1",["username","password"],["Karen","asdfgd"]);
 db.createTable("user2",["username","password"],["Vazgen","aghsdfgd"]);
 
-const a = new Table();
-a.add(1,"Vazgenchik","aedaef");
-a.update(1,"Gagik","aedefa");
-a.delete(2);
+db.getTable('user').add(1, 'a', 'b')
 
-
+db.add(1,"Vazgenchik","aedaef");
+// a.update(1,"Gagik","aedefa");
+// a.delete(2);
