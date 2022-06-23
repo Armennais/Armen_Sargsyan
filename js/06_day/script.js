18e386df6ebef0950bb35d39c275cf73e48bcad4
Armennais <armensargsyan1920@gmail.com>
2022-06-23 22:26:50 +0400

    Added mamber guess game

--- js/06_day/script.js
+++ js/06_day/script.js
@@ -1,64 +1,61 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
 const winLose = document.querySelector('#winLose')
 const inputNumber = document.querySelector('.inp_panel')
 const help = document.querySelector('#help')
 const again = document.querySelector('#again')
 const count = document.querySelector("#countValue");
 const highValue = document.querySelector("#highScoreValue");
-const wrong = document.querySelector("#wrongAnswers");
-
+console.log(randomNumber);
 let highScore = 20;
-let gethHighScoreValue = sessionStorage.getItem('highScore')
-if(gethHighScoreValue == null){
-    sessionStorage.setItem("highScore",String(0))
-    highValue.innerHTML = sessionStorage.getItem('highScore')
+let gethHighScoreValue = sessionStorage.getItem('highScore');
 
-}else{
-    sessionStorage.setItem("highScore",gethHighScoreValue)
-    highValue.innerHTML = sessionStorage.getItem('highScore')
+if (gethHighScoreValue == null) {
+    sessionStorage.setItem("highScore", String(0));
+    highValue.innerHTML = sessionStorage.getItem('highScore');
+
+} else {
+    sessionStorage.setItem("highScore", gethHighScoreValue);
+    highValue.innerHTML = sessionStorage.getItem('highScore');
 }
-console.log(highValue)
-function check(number,ramdomN){
-    
-    return Math.abs(number - ramdomN) > 10
+console.log(highValue);
+
+function check(number, ramdomN) {
+    return Math.abs(number - ramdomN) > 10;
 }
 
-function guessNumber(number){
+function guessNumber(number) {
     if (number == randomNumber) {
         inputNumber.disabled = true;
-        winLose.innerText = "You Win"
-        if(highScore > Number(sessionStorage.getItem('highScore'))){
-        sessionStorage.setItem("highScore",String(highScore))
-        highValue.innerHTML = sessionStorage.getItem('highScore')
+        winLose.innerText = "You Win";
+        if (highScore > Number(sessionStorage.getItem('highScore'))) {
+            sessionStorage.setItem("highScore", String(highScore));
+            highValue.innerHTML = sessionStorage.getItem('highScore');
         }
         wrong.innerText = "";
-    }else if(count.innerHTML == 0){
+    } else if (count.innerHTML == 0) {
         inputNumber.disabled = true;
-        winLose.innerText = "You Lose"
-    }else {
-        wrong.innerText = "wrong:";
-        count.innerHTML -= 1; 
+        winLose.innerText = "You Lose";
+    } else {
+        count.innerHTML -= 1;
         highScore -= 1;
-        if(number > randomNumber && check(number,randomNumber)) {
-            help.innerText = "Super high"
-        }else if(number < randomNumber && check(number,randomNumber)) {
-            help.innerText = "Super Low"
-        }else if(number > randomNumber){
-            help.innerText = "high"
-        }else if(number < randomNumber){
-            help.innerText = "Low"
+        if (number > randomNumber && check(number, randomNumber)) {
+            help.innerText = "Super high";
+        } else if (number < randomNumber && check(number, randomNumber)) {
+            help.innerText = "Super Low";
+        } else if (number > randomNumber) {
+            help.innerText = "high";
+        } else if (number < randomNumber) {
+            help.innerText = "Low";
         }
     }
 }
-inputNumber.addEventListener('keydown', function(event){
-	if(event.key === "Enter"){
-        guessNumber(inputNumber.value)
-        inputNumber.value = ''
-	}
+inputNumber.addEventListener('keydown', function (event) {
+    if (event.key === "Enter") {
+        guessNumber(inputNumber.value);
+        inputNumber.value = '';
+    }
 });
-again.addEventListener('click',function(){
+again.addEventListener('click', function () {
     inputNumber.disabled = false;
     return location.reload();
-
 })
