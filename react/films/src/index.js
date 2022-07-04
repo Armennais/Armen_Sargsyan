b8a66b0b5bea6cbf736e5c63d7785aecc83178fe
Armennais <armensargsyan1920@gmail.com>
2022-07-04 22:23:26 +0400

    Added navbar

--- react/films/src/index.js
+++ react/films/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 import './styles/index.css';
 import AppFC from './App.js';
-
+import Main from './main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppFC />
+    
+    <BrowserRouter >
+        <nav className='navbar'>
+            <Link to="/">HOME</Link>
+            <Link to="/films">FILMS</Link>
+        </nav>
+    <Routes>
+        <Route path = '/' element={<Main/>}/>
+        <Route path='/films' element={<AppFC/>}/>
+    </Routes>
+    </BrowserRouter>
+   
 );
 
---
+++ react/films/src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import './styles/Main.css';
+const Main = () => {
+    return(
+        <div className="main">
+           <p> WELLCOME!!!</p>
+        </div>
+    )
+}
+export default Main
